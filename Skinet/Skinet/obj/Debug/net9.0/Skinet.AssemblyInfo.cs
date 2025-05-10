54db724eda57292c2ffe736ccffee073188691ec
omarsilvayepes <omar.yepes@hotmail.com>
2025-05-10 16:19:02 -0500

    setup initial angular project with tailwind and angular material
    add header componet
    
    add sort, search ,filter ,pagination to shop products app

--- Skinet/Skinet/obj/Debug/net9.0/Skinet.AssemblyInfo.cs
+++ Skinet/Skinet/obj/Debug/net9.0/Skinet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Skinet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+471ea8a8ea438719f9b9f55145538109922f71b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab37ed0ae2aba0785aff92b3a813185155b4d856")]
 [assembly: System.Reflection.AssemblyProductAttribute("Skinet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Skinet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ client/src/app/shared/models/product.ts
@@ -0,0 +1,10 @@
+export type product={
+    id:number;
+    name:string;
+    description:string;
+    price:number;
+    pictureUrl:string;
+    type:string;
+    brand:string;
+    quantityInStock:number;
+}
